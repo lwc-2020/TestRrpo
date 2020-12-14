0fc4433860828b14acc17b43648ef33a11380bb4
lwc-2020 <1657770704@qq.com>
2020-12-14 20:50:24 +0800

    修改了controller

--- src/com/icis/Demo1.java
+++ src/com/icis/Demo1.java
@@ -3,5 +3,6 @@ package com.icis;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("sadasdasdda");
+        System.out.println("hahahahhahahahah");
     }
 }
